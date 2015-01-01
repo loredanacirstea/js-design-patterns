2a74c0158c54c4dea5b5854e0886a7a813306f6e
Loredana Cirstea <loredana.cirstea@gmail.com>
2015-01-01 18:36:41 +0200

    ini

---
+++ Model/loretek/design_patterns/creational/prototype/Prototype.js
@@ -0,0 +1,25 @@
+var Prototype = Class({
+    create: function() {
+        this.created = true;
+    }
+    Clone: function(){
+    }
+});
+
+var ConcretePrototype1 = Prototype.extend({
+	create: function() {
+        this.created = true;
+    }
+    Clone: function(){
+    	return this;
+    }
+});
+
+var ConcretePrototype2 = Prototype.extend({
+	create: function() {
+        this.created = true;
+    }
+    Clone: function(){
+    	return this;
+    }
+});
