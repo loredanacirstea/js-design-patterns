ebebe9da2b63aa3deed37e8bc3fedd187cae1fee
Loredana Cirstea <loredana.cirstea@gmail.com>
2015-01-06 15:06:03 +0200

    AbstractFactory ver 0.9

--- Model/loretek/design_patterns/creational/abstract_factory/AbstractFactory.js
+++ Model/loretek/design_patterns/creational/abstract_factory/AbstractFactory.js
@@ -1,49 +1,109 @@
-//var Class = require('../jsclass/src/js.class');
-
-var AbstractProductA = Class({
-    create: function() {
-        this.created = true;
-    }
-});
-
-var AbstractProductB = Class({
-    create: function() {
-        this.created = true;
-    }
-});
-
 var AbstractFactory = Class({
-    create: function() {
-        this.created = true;
+    create: function(factory) {
+        facade.log("AbstractFactory class created");
+        this.name = factory;
+        switch(factory){
+            case "ConcreteFactory1":
+                this.factory = ConcreteFactory1.instance();
+                break;
+            case "ConcreteFactory2":
+                this.factory = ConcreteFactory2.instance();
+                break;
+        }
     },
     createProductA: function(){
-        //this.productA = new AbstractProductA();
+        facade.log("AbstractFactory calls " + this.name + " to createProductA");
+        return this.factory.createProductA();
     },
     createProductB: function(){
-        //this.productB = new AbstractProductB();
-    }
-});
-
-var ProductA = AbstractProductA.extend({
-    create: function() {
-        this.created = true;
-    }
-});
-
-var ProductB = AbstractProductB.extend({
-    create: function() {
-        this.created = true;
+        facade.log("AbstractFactory calls " + this.name + " to createProductB");
+        return this.factory.createProductB();
     }
 });
 
 var ConcreteFactory1 = AbstractFactory.extend({
-    createProductA: function(){
-        this.product = new ProductA();
+    singleton: true,
+    create: function(){
+        facade.log("ConcreteFactory1 class created");
+    },
+    createProductA: function(){        
+        facade.log("ProductA created by ConcreteFactory1");
+        return new AbstractProductA("ProductA1");
+    },
+    createProductB: function(){
+        facade.log("ProductB created by ConcreteFactory1");
+        return new AbstractProductB("ProductB1");
     }
 });
 
 var ConcreteFactory2 = AbstractFactory.extend({
+    singleton: true,
+    create: function(){
+        facade.log("ConcreteFactory2 class created");
+    },
+    createProductA: function(){
+        facade.log("ProductA created by ConcreteFactory2");
+        return new AbstractProductA("ProductA2");
+    },
     createProductB: function(){
-        this.product = new ProductB();
+        facade.log("ProductB created by ConcreteFactory2");
+        return new AbstractProductB("ProductB2");
+    }
+});
+
+var AbstractProductA = Class({
+    create: function(product) {
+        facade.log("AbstractProductA class created");
+        this.product = null;
+        switch(product){
+            case "ProductA1":
+                this.product = new ProductA1;
+                break;
+            case "ProductA1":
+                this.product = new ProductA2;
+                break;
+        }
+        return this.product;
+    }
+});
+
+var AbstractProductB = Class({
+    create: function(product) {
+        facade.log("AbstractProductB class created");
+        this.product = null;
+        switch(product){
+            case "ProductB1":
+                this.product = new ProductB1;
+                break;
+            case "ProductB1":
+                this.product = new ProductB2;
+                break;
+        }
+        return this.product;
+    }
+});
+
+
+var ProductA1 = AbstractProductA.extend({
+    create: function() {
+        facade.log("ProductA1 class created");
+    }
+});
+
+var ProductA2 = AbstractProductA.extend({
+    create: function() {
+        facade.log("ProductA2 class created");
+    }
+});
+
+var ProductB1 = AbstractProductB.extend({
+    create: function() {
+        facade.log("ProductB1 class created");
+    }
+});
+
+var ProductB2 = AbstractProductB.extend({
+    create: function() {
+        facade.log("ProductB2 class created");
     }
 });
